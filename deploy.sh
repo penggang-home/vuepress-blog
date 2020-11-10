894acd1bf639f47520c1bcb81064ce23d922fab6
penggang <2845486124@qq.com>
2020-11-10 20:41:38 +0800

    使用 HTTPS 发布到GitHub

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:penggang-home.github.io.git master
+git push -f  https://github.com/penggang-home/penggang-home.gitub.io.git master:gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # 请把 penggang-home/blog-two.git 换成你的项目地址
