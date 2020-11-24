165cfc36e7d326a8e68890ea117788b64cbecef4
penggang <2845486124@qq.com>
2020-11-24 11:38:55 +0800

    delete docs

--- docs/.vuepress/private/private.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  appId: "X3BngL3TJOaKhbqvMkYD9DkL-gzGzoHsz",
-  appKey: "kaCkhB2ENIxkpA5l3NMEbvxy",
-}
--- docs/README.md
+++
@@ -1,5 +0,0 @@
----
-home: true
-bgImage: "/images/bg-logo.svg"
-bgImageStyle: { height: "100vh" }
----
